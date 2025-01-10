0db7cbb5fa82a3eb01745d5a14ed252c6a260877
Mikorra <163505003+Mikorra@users.noreply.github.com>
2025-01-10 09:12:04 +0200

    Added boilerplate to index.html and added pseudocode and consolelog to javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//Pseudocode
+//create variable for rock
+//create variable for paper
+//create variable for scissors
+//create three buttons for each rock, paper and scissors
+//create a function which tells that rock beats scissors, paper beats rock and scissors beat paper.
+//ask input from the user for either rock, paper or scissors
+//opponent(computer) randomly chooses output for either rock, paper or scissors
+//print what user chose and what the opponent chose
+//print the result of the game(function)
+//print victory or lose message?
+//print a start a new game button?
+//reset the game and start again?
+
+console.log("Hello World")
